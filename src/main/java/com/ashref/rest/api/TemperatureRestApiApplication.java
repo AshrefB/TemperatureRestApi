9c8c88ba90d264bac7980003697758a3e64a6908
Achref Bouadila <a_bouadila@live.fr>
2020-02-25 21:29:30 +0100

    first commit

---
+++ src/main/java/com/ashref/rest/api/TemperatureRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ashref.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureRestApiApplication.class, args);
+	}
+
+}
